984746055a9a4c544aacf6b5af15ae4f00bf7c65
go95 <go9513@gmail.com>
2018-03-18 20:45:23 +0300

    Update problem_2.3.R

--- problem_2.3.R
+++ problem_2.3.R
@@ -1,76 +1,81 @@
-library(mvtnorm)
-library(grf)
-library(zeallot)
-library(pbapply)
+#Random forest
 
 set.seed(1234)
 
-n <- 1000
-p <- 20 #this is just for compatability with bonus task
-pi <- 0.5
-X.test <- matrix(0, 101, p)
-X.test[,1] <- seq(-2, 2, length.out = 101)
+# creating constants in 0)
+n<-1000
+p<-10
+pi<-0.5
+X.test = matrix(0, 101, p)
+X.test[,1] = seq(-2, 2, length.out = 101)
 
-generate_data <- function() { 
+#function generating data
+DGP<-function(n){
+  # X - design matrix 
+  library(mvtnorm)
   Sigma <- diag(p)
   X <- rmvnorm(n, sigma = Sigma, method = "chol")
-  W <- rbinom(n=n,size = 1, prob = pi)
+  
+  # treatment variable W 
+  W<-rbinom(n=n,size = 1, prob = pi)
   
   # generate  outcome variable
-  eps <- rnorm(n)
-  Y <- pmax(0, X[,1]) * W + X[,2] + pmax(0, X[,2]) + eps
+  eps<-rnorm(n, 0, 1)
+  Y<-pmax(0,X[,1])*W+X[,2]+pmax(0, X[,2])+eps
   
-  return(list(Y = Y, W = W, X = X))
+  return(list(Y=Y,W=W,X=X))
+}
+#data<-DGP(n)
+
+
+taufunction<-function(data){
+library(grf)
+tau.forest <- causal_forest(data$X, data$Y, data$W, num.trees = 4000)
+tau.hat <- predict(tau.forest, X.test, estimate.variance = TRUE)
+sigma.hat <- sqrt(tau.hat$variance.estimates)
+
+tau_hat<-tau.hat$predictions
+CI<- list(L=tau.hat$predictions - qnorm(0.95) * sigma.hat, U=tau.hat$predictions + qnorm(0.95) * sigma.hat)
+
+#true effects
+tau_true<-pmax(0,X.test[,1])
+
+return(list(tau_true=tau_true,tau_hat=tau_hat,CI=CI))
 }
 
+#R<-taufunction(data)
 
-estimate_effect <- function(data, p){
+Indicator<-function(x, min, max)
+{ 
+  as.numeric(x >= min) * as.numeric(x <= max)
+}
 
-  tau.forest <- causal_forest(data$X[,1:p], data$Y, data$W, num.trees = 4000)
-  tau.hat <- predict(tau.forest, X.test[,1:p], estimate.variance = TRUE)
-  sigma.hat <- sqrt(tau.hat$variance.estimates)
+Errorfunction<-function(R){
+  SE=(R$tau_true-R$tau_hat)^2
+  I<-matrix(Indicator(R$tau_true, R$CI$L,R$CI$U), nrow = 101, ncol = 1)
+  return(cbind(SE,I))
+}
+
+#Errorfunction(R)
+
+
+#Simulation
+k=300
+
+Z<-matrix(data=0,nrow = 101,ncol=k)
+I<-matrix(data=0,nrow = 101,ncol=k)
+for (j in 1:k) {
   
-  tau_hat <- tau.hat$predictions
-  CI <- c(tau.hat$predictions - qnorm(0.95) * sigma.hat, tau.hat$predictions + qnorm(0.95) * sigma.hat)
-  
-  tau_true <- pmax(0,X.test[,1])
-  
-  return(list(tau_true = tau_true, tau_hat = tau_hat, CI = CI))
+  P<-Errorfunction(taufunction(DGP(n)))
+  Z[,j]<-P[,1]
+  I[,j]<-P[,2]
 }
 
+meanZ<-rowMeans(Z, dims = 1, na.rm = TRUE)
+meanI<-rowMeans(I, dims = 1, na.rm = TRUE)
 
-squared_error <- function(estimates) {
-  se = (estimates$tau_true - estimates$tau_hat)^2
-  I <- estimates$tau_true >= estimates$CI[1] && estimates$tau_true <= estimates$CI[1]
-  return(c(se, I))
-}
+plot(X.test[,1],y =  meanZ,  xlab="x", ylab="Error ", pch=19, main = "p=10")
 
-
-
-simulation_instance <- function(place_holder, p) {
-  data <- generate_data()
-  params <- estimate_effect(data, p)
-  return(squared_error(params))
-}
-
-simulation <- function(iterations, p) {
-  Z <- matrix(0, nrow = iterations, ncol=3)
-  Z <- pbapply(Z, 1, simulation_instance, p=p)
-  return(t(Z))
-}
+plot(X.test[,1],y =  meanI,  xlab="x", ylab="Coverage ", pch=19, main = "p=10")
 
 
-assess_simulation <- function() {
-  for p in c(1, 3, 20) {
-    result <- numeric()
-    op <- pboptions(type = "timer")
-    Z <- simulation(300, p)
-    pboptions(op)
-    means <- colMeans(Z)
-    plot(X.test[,1], result)
-    result <- rbind(result, means)
-  }
-  return(result)
-}
-
-result <- assess_simulation()
